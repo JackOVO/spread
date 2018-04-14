18a5a8fc0225ad02206ee6024d530140e7c99ca5
jackov@126.com <jackov@163.com>
2018-04-15 01:24:01 +0800

    控制台二维码镯子临时适配

--- web/src/views/QR.vue
+++ web/src/views/QR.vue
@@ -7,6 +7,7 @@
 <template>
   <Row>
     <Col>
+      衣服地址:
       <Input
         v-model="domain"
         placeholder="请输入域名">
@@ -16,6 +17,20 @@
     </Col>
     <Col>
       <qr-code :text="address" class="qr-wrapper" style="margin-top: 20px;" />
+    </Col>
+  </Row>
+  <Row>
+    <Col>
+      镯子地址:
+      <Input
+        v-model="domain"
+        placeholder="请输入域名">
+        <span slot="prepend">http://</span>
+        <span slot="append">/api/link/tmp/{{ username }}</span>
+      </Input>
+    </Col>
+    <Col>
+      <qr-code :text="bracelet" class="qr-wrapper" style="margin-top: 20px;" />
     </Col>
   </Row>
   
@@ -35,6 +50,9 @@
     computed: {
       address: function() {
         return `http://${this.domain}/api/link/${this.username}`
+      },
+      bracelet: function() {
+        return `http://${this.domain}/api/link/tmp/${this.username}`
       }
     }
   }
