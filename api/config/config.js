009082a3267cb80e5b09cfb9ea954ace68491db1
jackov@126.com <jackov@163.com>
2018-05-19 17:34:07 +0800

    更改部署商品id

--- api/config/config.js
+++ api/config/config.js
@@ -2,8 +2,6 @@ const path = require('path');
 const rootPath = path.normalize(__dirname + '/..');
 const env = process.env.NODE_ENV || 'development';
 
-console.info(env);
-
 const config = {
   development: {
     root: rootPath,
@@ -11,7 +9,26 @@ const config = {
       name: 'spread'
     },
     port: process.env.PORT || 3000,
-    db: 'mongodb://localhost/spread-development'
+    db: 'mongodb://localhost/spread-development',
+    viewMap: {
+      wear: {
+        value: 'old_wear',
+        title: '新★ 正 品 ★',
+        content: '新★ 正 品 ★',
+        product: '5ae70de8c4041ce7d90e4372'
+      },
+      bracelet: {
+        value: 'old_bracelet',
+        title: '新★ 正 品 ★',
+        content: '新★ 正 品 ★',
+        product: '5ae8421a9e4f9d6cd056d4be'
+      },
+      card: {
+        value: 'card',
+        title: '微信名片',
+        content: '微信名片'
+      }
+    }
   },
 
   test: {
@@ -29,7 +46,26 @@ const config = {
       name: 'spread'
     },
     port: process.env.PORT || 3000,
-    db: 'mongodb://localhost/spread-developmentV2'
+    db: 'mongodb://localhost/spread-developmentV2',
+    viewMap: {
+      wear: {
+        value: 'old_wear',
+        title: '新★ 正 品 ★',
+        content: '新★ 正 品 ★',
+        product: '5aeb0cda9803f33c670a8dac'
+      },
+      bracelet: {
+        value: 'old_bracelet',
+        title: '新★ 正 品 ★',
+        content: '新★ 正 品 ★',
+        product: '5aeb0cbd9803f33c670a8dab'
+      },
+      card: {
+        value: 'card',
+        title: '微信名片',
+        content: '微信名片'
+      }
+    }
   }
 };
 
@@ -39,26 +75,6 @@ config[env].oss = {
   ak: {
     accessKeyId: 'LTAIhUmdS0sYXq7M',
     accessKeySecret: 'wkk1gxD8pfGfr9NydSLGIE4wfWxc2s'
-  }
-};
-
-config[env].viewMap = {
-  wear: {
-    value: 'old_wear',
-    title: '新★ 正 品 ★',
-    content: '新★ 正 品 ★',
-    product: '5ae70de8c4041ce7d90e4372' // 线 5aeb0cda9803f33c670a8dac
-  },
-  bracelet: {
-    value: 'old_bracelet',
-    title: '新★ 正 品 ★',
-    content: '新★ 正 品 ★',
-    product: '5ae8421a9e4f9d6cd056d4be' // 5aeb0cbd9803f33c670a8dab
-  },
-  card: {
-    value: 'card',
-    title: '微信名片',
-    content: '微信名片'
   }
 };
 
